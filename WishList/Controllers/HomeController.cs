44f15ed96a451de2bc98f8ecc00ae80fe35166c2
mhadamji <mhadamji@hotmail.com>
2022-04-10 00:46:35 +0100

    error

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -8,5 +8,9 @@ namespace WishList.Controllers
         {
             return View("Index");
         }
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
     }
 }
