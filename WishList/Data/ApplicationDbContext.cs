12fb3ba49888c751c2264169ba3e05070a1872ed
mhadamji <mhadamji@hotmail.com>
2022-04-10 11:01:38 +0100

    DbSet

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Item> items;
     }
 }
